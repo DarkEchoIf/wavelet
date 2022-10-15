79aa7fcfd206ab31beea3c94a929c6b81c8a9be7
DarkEchoIf <niuweizhedavid@gmail.com>
2022-10-14 20:44:42 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -3,9 +3,9 @@ import java.net.URI;
 import java.util.ArrayList;
 
 class SearchEngineHandler implements URLHandler {
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayList<String> searchList = new ArrayList<String>();
     public String handleRequest(URI url) {
-        ArrayList<String> list = new ArrayList<String>();
-        ArrayList<String> searchList = new ArrayList<String>();
 
         if (url.getPath().equals("/")) {
             return "We have these strings stored: " + list;
@@ -15,9 +15,9 @@ class SearchEngineHandler implements URLHandler {
             String[] parameters = url.getQuery().split("=");
             if (parameters[0].equals("s")) {
                 list.add(parameters[1]);
-                return "The list has been updated";
+                return "" + list;
             }
-                return "Invalid Operation";
+            return "Invalid Operation";
         }
             else {
                 if (url.getPath().contains("/search")){
@@ -38,7 +38,7 @@ class SearchEngineHandler implements URLHandler {
     }
 
 
-public class SearchEngine {
+class SearchEngine {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
@@ -47,6 +47,6 @@ public class SearchEngine {
 
         int port = Integer.parseInt(args[0]);
 
-        Server.start(port, new Handler());
+        Server.start(port, new SearchEngineHandler());
     }
 }
